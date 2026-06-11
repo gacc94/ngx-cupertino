0ae6c5632310eb98f83fc8eabcf0f1eb2f6410ea
Gustavo Caqui <gustavocaqui94@gmail.com>
2026-06-11 01:41:46 -0500

    fix(ui): remove @starting-style causing jsdom parse error in CI, sync template classes to cup-btn-*

--- libs/ui/src/lib/button/cup-button.ts
+++ libs/ui/src/lib/button/cup-button.ts
@@ -30,14 +30,14 @@ import { CupIcon } from "@ngx-cupertino/icons";
     },
     template: `
         @if (icon() && iconPosition() === 'start') {
-            <cup-icon [name]="icon()!" class="icon" />
+            <cup-icon [name]="icon()!" class="cup-btn-icon" />
         }
-        <span class="label"><ng-content /></span>
+        <span class="cup-btn-label"><ng-content /></span>
         @if (icon() && iconPosition() === 'end') {
-            <cup-icon [name]="icon()!" class="icon" />
+            <cup-icon [name]="icon()!" class="cup-btn-icon" />
         }
         @if (loading()) {
-            <span class="spinner"></span>
+            <span class="cup-btn-spinner"></span>
         }
     `,
     styleUrl: "./cup-button.scss",
